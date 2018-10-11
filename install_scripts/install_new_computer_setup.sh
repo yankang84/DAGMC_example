476ec42d7cd10e3ce2a5d294ae9f1027be867085
Shimwell, Jonathan <jonathan.shimwell@ukaea.uk>
2018-10-11 18:23:53 +0100

    tet mesh and .sat file support added

---
+++ install_scripts/install_new_computer_setup.sh
@@ -0,0 +1,195 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get dist-upgrade
+
+sudo apt -y install gcc
+sudo apt -y install make
+
+sudo apt-get install -y devscripts build-essential libsqlite3-dev sqlite3 bzip2 libbz2-dev zlib1g-dev libssl-dev openssl libgdbm-dev libgdbm-compat-dev liblzma-dev libreadline-dev libncursesw5-dev libffi-dev uuid-dev
+sudo apt install -y curl
+
+#python3
+sudo apt install -y python3-dev python3-setuptools python3-pip ipython3 python3-tk
+sudo python3 -m pip install jupyterlab
+sudo jupyter labextension install @jupyter-widgets/jupyterlab-manager
+
+
+sudo apt-get install -y libncursesw5-dev libgdbm-dev libc6-dev
+sudo apt-get install -y zlib1g-dev libsqlite3-dev tk-dev
+sudo apt-get install -y libssl-dev openssl
+sudo apt-get install -y libffi-dev
+
+#build python3 from source
+# https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tgz
+#     ./configure
+#     make
+#     make test
+#     sudo make install
+
+
+#sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-add-repository "deb https://download.sublimetext.com/ apt/stable/"
+sudo apt -y install sublime-text
+
+#atom
+curl -sL https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+sudo apt-get update
+sudo apt-get install -y atom
+
+# mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+python3 -m pip install pymongo --user
+sudo mkdir /data
+sudo mkdir /data/db
+
+#react
+sudo apt-get -y install nodejs
+sudo apt -y install npm
+# npm install -g create-react-app
+# create-react-app my-app
+npm install --save reactstrap react react-dom
+npm install --save react react-dom 
+npm install --save react-bootstrap
+npm install react-select
+npm install react-table
+npm install react-plotly.js plotly.js
+# create-react-app my-app
+# npm start
+
+# allows usb drives with exfat format to be accessed
+sudo apt install exfat-fuse exfat-utils
+
+
+
+
+
+pip3 install ipywidgets --user
+pip3 install pandas --user
+pip3 install matplotlib --user
+pip3 install plotly --user
+pip3 install dash==0.26.3 --user
+pip3 install dash-html-components==0.12.0 --user
+pip3 install dash-core-components==0.28.0 --user
+
+wget https://download.jetbrains.com/python/pycharm-community-2018.2.3.tar.gz
+
+
+
+
+#DAGMC
+cd $HOME
+mkdir dagmc_bld
+cd dagmc_bld
+mkdir -p $HOME/dagmc_bld/HDF5/bld
+cd $HOME/dagmc_bld/HDF5
+wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8/hdf5-1.8.13/src/hdf5-1.8.13.tar.gz
+tar -xzvf hdf5-1.8.13.tar.gz
+ln -s hdf5-1.8.13 src
+cd $HOME/dagmc_bld/HDF5/bld
+../src/configure --enable-shared                    --prefix=$HOME/dagmc_bld/HDF5
+make
+make check
+make install
+cd $HOME/dagmc_bld
+mkdir -p MOAB/bld
+cd $HOME/dagmc_bld/MOAB
+git clone https://bitbucket.org/fathomteam/moab
+cd $HOME/dagmc_bld/MOAB/moab
+git checkout Version5.0
+
+#missing from instructions
+sudo apt-get install -y libtool
+#missing from instructions
+sudo apt install -y autoconf
+
+autoreconf -fi
+
+#missing from instructions
+sudo apt-get install devscripts
+#missing from instructions
+sudo apt-get install gawk
+#missing from instructions
+#sudo apt-get install gfortran-6
+
+cd $HOME/dagmc_bld/MOAB
+ln -s moab src
+cd bld
+../src/configure --enable-optimize --enable-shared --disable-debug --with-hdf5=$HOME/dagmc_bld/HDF5 --prefix=$HOME/dagmc_bld/MOAB
+apt-get install libblas-dev libatlas-dev liblapack-dev
+
+
+#missing from instructions, not found in ubuntu 8.04
+sudo apt-get install -y libblas-dev 
+sudo apt-get install -y liblapack-dev
+
+
+make
+make check
+make install
+
+#add to path
+export PATH=$PATH:$HOME/dagmc_bld/HDF5/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/dagmc_bld/HDF5/lib
+
+export PATH=$PATH:$HOME/dagmc_bld/MOAB/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/dagmc_bld/MOAB/lib
+
+
+
+
+
+  
+
+ sudo add-apt-repository ppa:alexlarsson/flatpak
+    
+
+ sudo apt update
+      
+ sudo apt install flatpak
+
+ sudo apt install gnome-software-plugin-flatpak
+
+ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    
+ flatpak install https://flathub.org/repo/appstream/org.gimp.GIMP.flatpakref
+
+
+sudo apt install python3-pytest
+
+
+pip3 install pytest
+
+
+sudo apt-get update
+
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg 
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+apt-cache madison docker-ce
+sudo apt-get install docker-ce=18.04.0~ce-0~ubuntu .
+sudo docker run hello-world
+
+
+
+
+sudo apt -y install git
+# ssh-keygen -t rsa -b 4096 -C "mail@jshimwell.com"
+# eval "$(ssh-agent -s)"
+# ssh-add ~/.ssh/id_rsa
+# sudo apt-get install xclip
+# xclip -sel clip < ~/.ssh/id_rsa.pub
+
+
+# freecad
+sudo add-apt-repository ppa:freecad-maintainers/freecad-stable
+sudo apt-get update 
+sudo apt-get install freecad freecad-doc && sudo apt-get upgrade 
--- make_dagmc_geometry_with_material_tags.py
+++ make_dagmc_geometry_with_material_tags.py
@@ -11,7 +11,11 @@ def find_number_of_volumes_in_each_step_file(input_locations):
     for entry in input_locations:
       current_vols =cubit.parse_cubit_list("volume", "all")
       print(entry['filename'])
-      cubit.cmd('import step "' + entry['filename'] + '" separate_bodies no_surfaces no_curves no_vertices heal group "'+str(entry['filename'])+'"')
+      if entry['filename'].endswith('.sat'):
+        import_type = 'acis'
+      if entry['filename'].endswith('.stp') or entry['filename'].endswith('.step'):
+        import_type = 'step'
+      cubit.cmd('import '+import_type+' "' + entry['filename'] + '" separate_bodies no_surfaces no_curves no_vertices heal group "'+str(entry['filename'])+'"')
       all_vols =cubit.parse_cubit_list("volume", "all")
       new_vols = set(current_vols).symmetric_difference(set(all_vols))
       new_vols = map(str, new_vols)
@@ -28,6 +32,7 @@ def find_number_of_volumes_in_each_step_file(input_locations):
     return input_locations
 
 def create_graveyard():
+  #this could be replaced with a bounding box
   cubit.cmd('brick x 110')
   cubit.cmd('brick x 111')
   cubit.cmd('subtract volume 1 from volume 2')
@@ -89,7 +94,7 @@ def find_reflecting_surfaces(surface_info_dict):
             surface_info_dict[surface_id]= {'reflector':True}
     return surface_info_dict
 
-# os.system('python3 make_materials.py')
+
 create_graveyard()
 wedge_surface_info_dict = load_reflecting_wedge(input_location_wedge="reflecting_wedge.stp")
 #common_wedge_volume = load_common_wedge(input_location_wedge="common_wedge.stp")
@@ -98,9 +103,15 @@ wedge_surface_info_dict = load_reflecting_wedge(input_location_wedge="reflecting
 with open('model_description.json') as f:
     geometry_details = byteify(json.load(f))
 
+def scale_geometry(geometry_details):
+  for entry in geometry_details:
+    if 'scale' in entry.keys():
+      cubit.cmd('volume ' +' '.join(entry['volumes'] + ' scale ')
 
 
 geometry_details = find_number_of_volumes_in_each_step_file(geometry_details)
+
+scale_geometry(geometry_details)
 
 print('geometry_details',geometry_details)
 
@@ -113,10 +124,8 @@ for entry in geometry_details:
     # Available Colors https://www.csimsoft.com/help/appendix/available_colors.htm
     cubit.cmd('Color volume '+' '.join(entry['volumes'])+ ' '+ entry['color'])
 
-#volume in group 12 copy rotate 45 about z repeat 7
 
 
-cubit.cmd('vol all scale 100')
 cubit.cmd('imprint body all')
 #cubit.cmd('merge tolerance 1.e-4')
 cubit.cmd('merge vol all group_results tolerance 1.e-4')
@@ -136,13 +145,24 @@ cubit.cmd('set attribute on')
 
 cubit.cmd('export dagmc "geometry_with_material_tags.h5m" faceting_tolerance 1.0e-2')
 
+current_vols =cubit.parse_cubit_list("volume", "all")
 
-
-#cubit.cmd('delete mesh')
-#cubit.cmd('delete volume 3 4')
+cubit.cmd('delete mesh')
+cubit.cmd('delete volume 3 4')
+#
 #cubit.cmd('Trimesher volume gradation 1.3')
 #cubit.cmd('volume all size auto factor 5')
-#cubit.cmd('mesh volume all')
+#
+
+for volume in current_vols:
+  cubit.cmd('volume all scheme tetmesh proximity layers off geometric sizing on')
+  cubit.cmd('mesh volume '+str(volume))
+
+for volume in current_vols:
+  print('volume id ', volume, ' is meshed = ', cubit.is_meshed('vol',volume))
+
+cubit.cmd('save as "tetmesh.cub" overwrite')
 
 # additional steps needed for unstructured mesh https://svalinn.github.io/DAGMC/usersguide/tally.html
 os.system('rm *.jou')
+
