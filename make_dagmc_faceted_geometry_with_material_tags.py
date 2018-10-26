19f3ef19e07879bd1b4e18761bc6d4d6ef4526f8
Jonathan Shimwell <Jonathan.Shimwell@ukaea.uk>
2018-10-26 18:23:38 +0100

    fixed grouping name when loading load bug

--- make_dagmc_faceted_geometry_with_material_tags.py
+++ make_dagmc_faceted_geometry_with_material_tags.py
@@ -7,31 +7,37 @@ cubit.cmd('reset')
 def find_number_of_volumes_in_each_step_file(input_locations):
     body_ids=''
     volumes_in_each_step_file=[]
-    starting_group_id = 5
+    #all_groups=cubit.parse_cubit_list("group","all")
+    #starting_group_id = len(all_groups)
     for entry in input_locations:
+      #starting_group_id = starting_group_id +1
       current_vols =cubit.parse_cubit_list("volume", "all")
       print(entry['filename'])
       if entry['filename'].endswith('.sat'):
         import_type = 'acis'
       if entry['filename'].endswith('.stp') or entry['filename'].endswith('.step'):
         import_type = 'step'
-      cubit.cmd('import '+import_type+' "' + entry['filename'] + '" separate_bodies no_surfaces no_curves no_vertices group "'+str(entry['filename'])+'"')
+      short_file_name = os.path.split(entry['filename'])[-1]
+      print('short_file_name',short_file_name)
+      #cubit.cmd('import '+import_type+' "' + entry['filename'] + '" separate_bodies no_surfaces no_curves no_vertices group "'+str(short_file_name)+'"')
+      cubit.cmd('import '+import_type+' "' + entry['filename'] + '" separate_bodies no_surfaces no_curves no_vertices ')
       all_vols = cubit.parse_cubit_list("volume", "all")
       new_vols = set(current_vols).symmetric_difference(set(all_vols))
       new_vols = map(str, new_vols)
       print('new_vols',new_vols,type(new_vols))
       current_bodies = cubit.parse_cubit_list("body","all")
       print('current_bodies',current_bodies)
-      volumes_in_group = cubit.cmd('volume in group '+str(starting_group_id))
-      print('volumes_in_group',volumes_in_group,type(volumes_in_group))
-      cubit.cmd('unite vol '+ ' '.join(new_vols)+ ' with vol '+' '.join(new_vols))
+      #volumes_in_group = cubit.cmd('volume in group '+str(starting_group_id))
+      #print('volumes_in_group',volumes_in_group,type(volumes_in_group))
+      if len(new_vols) > 1:
+        cubit.cmd('unite vol '+ ' '.join(new_vols)+ ' with vol '+' '.join(new_vols))
       all_vols = cubit.parse_cubit_list("volume", "all")
       new_vols_after_unite = set(current_vols).symmetric_difference(set(all_vols))
       new_vols_after_unite = map(str, new_vols_after_unite)      
       #cubit.cmd('group '+str(starting_group_id)+' copy rotate 45 about z repeat 7')
       entry['volumes']=new_vols_after_unite
+      cubit.cmd('group "'+short_file_name+'" add volume ' +' '.join(entry['volumes']))
       #cubit.cmd('volume in group '+str(starting_group_id)+' copy rotate 45 about z repeat 7')
-      starting_group_id = starting_group_id +1
     cubit.cmd('separate body all')
     return input_locations
 
@@ -84,7 +90,7 @@ def load_reflecting_wedge(input_location_wedge):
   print('surface_info_dict',surface_info_dict)
   cubit.cmd('group "mat:Vacuum" add volume '+ ' '.join(new_vols))
   cubit.cmd('volume '+' '.join(new_vols)+' visibility off')
-  return surface_info_dict
+  return surface_info_dict, new_vols[0]
 
 def load_common_wedge(input_location_wedge):
   current_vols =cubit.parse_cubit_list("volume", "all")
@@ -106,8 +112,8 @@ def byteify(input):
     else:
         return input
 
-def find_reflecting_surfaces(surface_info_dict):
-    surfaces_in_wedge_volume = cubit.parse_cubit_list("surface", " in volume 4")
+def find_reflecting_surfaces(surface_info_dict,wedge_volume):
+    surfaces_in_wedge_volume = cubit.parse_cubit_list("surface", " in volume "+str(wedge_volume))
     for surface_id in surface_info_dict.keys():
         if surface_info_dict[surface_id]['reflector']==True:
             print(surface_id, 'surface originally reflecting but does it still exist')
@@ -125,6 +131,7 @@ def scale_geometry(geometry_details):
 
 def color_geometry(geometry_details):
     for entry in geometry_details:
+       #cubit.cmd('group "'+os.path.split(entry['filename'])[-1]+'" add volume ' +' '.join(entry['volumes'])) can be performed here or in the file loading
        cubit.cmd('group "mat:'+entry['material']+'" add volume ' +' '.join(entry['volumes']))
        #cubit.cmd('unite vol '+ ' '.join(entry['volumes']))
        print(entry)
@@ -134,7 +141,7 @@ def color_geometry(geometry_details):
         cubit.cmd('Color volume '+' '.join(entry['volumes'])+ ' '+ entry['color'])    
 
 
-wedge_surface_info_dict = load_reflecting_wedge(input_location_wedge="reflecting_wedge.stp")
+wedge_surface_info_dict, wedge_volume = load_reflecting_wedge(input_location_wedge="reflecting_wedge.stp")
 #common_wedge_volume = load_common_wedge(input_location_wedge="common_wedge.stp")
 
 with open('model_description.json') as f:
@@ -154,8 +161,9 @@ cubit.cmd('imprint body all')
 cubit.cmd('merge vol all group_results')
 cubit.cmd('graphics tol angle 3')
 
-updated_wedge_surface_info_dict = find_reflecting_surfaces(wedge_surface_info_dict)
+updated_wedge_surface_info_dict = find_reflecting_surfaces(wedge_surface_info_dict,wedge_volume)
 print('wedge_surface_info_dict',wedge_surface_info_dict)
+print('wedge_volume',type(wedge_volume),wedge_volume)
 
 for surface_id in updated_wedge_surface_info_dict.keys():
   if updated_wedge_surface_info_dict[surface_id]['reflector'] == True:
